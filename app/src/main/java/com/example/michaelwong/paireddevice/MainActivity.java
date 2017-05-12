701dfd031c20ba72ec6172377604c71fe69b00fd
Michael Wong <michael1998@hotmail.ca>
2017-05-12 11:01:47 -0700

    Connection to Glass device
    must always open new ControlSuit before running

--- app/src/main/java/com/example/michaelwong/paireddevice/MainActivity.java
+++ app/src/main/java/com/example/michaelwong/paireddevice/MainActivity.java
@@ -4,6 +4,7 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.os.Bundle;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -15,21 +16,54 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Set;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String TAG = "TAG";
     private TextView text1;
     private Thread ConnectThread;
-
+    private Thread ManageThread;
     BluetoothAdapter mBluetoothAdapter;
     Set<BluetoothDevice> pairedDevices;
     private BluetoothDevice mmDevice;
+    private  BluetoothSocket mmSocket;
 
     private static final UUID insecureUUID = UUID.fromString("8ce255c0-200a-11e0-ac64-0800200c9a66");
+
+    private  class ManageThread extends Thread {
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+        private byte[] mmBuffer;
+        public ManageThread (){
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+            try {
+                tmpIn = mmSocket.getInputStream();
+            } catch (IOException e) {
+                Log.i(TAG, "Error occurred when creating input stream");
+            }
+            Log.i(TAG, "tmpIn try block done");
+            try {
+                tmpOut = mmSocket.getOutputStream();
+            } catch (IOException e) {
+                Log.i(TAG, "Error occurred when creating output stream");
+            }
+            Log.i(TAG, "tmpOut try block done");
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+        public void write(byte[] bytes) {
+            try {
+                mmOutStream.write(bytes);
+            } catch (IOException e) {
+                Log.i(TAG, "Error occurred when sending data");
+            }
+        }
+    }
     private class ConnectThread extends Thread {
-        private final BluetoothSocket mmSocket;
         Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
         public ConnectThread() {
             // Use a temporary object that is later assigned to mmSocket
@@ -38,52 +72,42 @@ public class MainActivity extends AppCompatActivity {
             if (pairedDevices.size() > 0) {
                 // There are paired devices. Get the name and address of each paired device.
                 for (BluetoothDevice device : pairedDevices) {
-                    if(device.getName() == "Glass 4012") {
-                        mmDevice = device;
-                    }
+                    mmDevice = device;
+                    Log.i(TAG, "Glass device: " + device.getName() + " initialized");
                 }
             }
-
+            Log.i(TAG, "Trying to connect to Glass");
             try {
                 // Get a BluetoothSocket to connect with the given BluetoothDevice.
                 // MY_UUID is the app's UUID string, also used in the server code.
                 tmp = mmDevice.createRfcommSocketToServiceRecord(insecureUUID);
             } catch (IOException e) {
-                text1.setText("failed to connect");
+                Log.e(TAG, "Socket's create() method failed", e);
             }
+            Log.i(TAG, "Connected to Glass");
             mmSocket = tmp;
-            text1.setText("Connected");
+            Log.i(TAG, "Connected Glass to mmSocket");
         }
 
-        public void run() {
-            // Cancel discovery because it otherwise slows down the connection.
-            mBluetoothAdapter.cancelDiscovery();
+        public void  run() {
+            Log.i(TAG, "Running connect thread");
 
             try {
                 // Connect to the remote device through the socket. This call blocks
                 // until it succeeds or throws an exception.
+                Log.i(TAG, "Running connect()");
                 mmSocket.connect();
             } catch (IOException connectException) {
+                Log.i(TAG, "Running connect() failed");
+
                 // Unable to connect; close the socket and return.
-                try {
-                    mmSocket.close();
-                } catch (IOException closeException) {
-                }
-                return;
             }
-
-            // The connection attempt succeeded. Perform work associated with
-            // the connection in a separate thread.
-        }
-
-        // Closes the client socket and causes the thread to finish.
-        public void cancel() {
-            try {
-                mmSocket.close();
-            } catch (IOException e) {
-            }
+            Log.i(TAG, "Running connect() completed with" + mmSocket.isConnected());
+            ManageThread = new ManageThread();
+            ManageThread.start();
         }
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -98,6 +122,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
